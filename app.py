73db5ed1d6999cd46a683df5cbb3ba9072f14916
VinayMettela <157690261+VinayMettela@users.noreply.github.com>
2024-07-31 12:42:00 +0000

    HTML , CSS

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-  return "hello world"
+  return render_template('Home.html')
+
+
 print(__name__)
 
-if __name__=="__main__":
+if __name__ == "__main__":
   # print("i am inside the if")
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0", debug=True)
