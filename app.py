96584471037a006a9cb459fd892cdf5b565f4bda
VinayMettela <157690261+VinayMettela@users.noreply.github.com>
2024-08-01 08:37:57 +0000

    Made some changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ JOBS=[
   {'id':1, 'title':'Data Analyst', 'location':'Bengaluru, India' , 'salary':'Rs. 10,00,000'},
   {'id':2, 'title':'Data Scientist', 'location':'Delhi, India' , 'salary': 'Rs. 15,00,000'},
 {'id':3, 'title':'Frontend Engineer', 'location':'Remote'},
-{'id':4, 'title':'Backend Engineer', 'location': 'San Francisco, USA' , 'salary': '120,0000'}
+{'id':4, 'title':'Backend Engineer', 'location': 'San Francisco, USA' , 'salary': '20,00000'}
 ]
 
 @app.route("/")
